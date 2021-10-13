900daed98602eb94489cbd564088db169b554f0d
SEUNG <app.lnob@gmail.com>
2021-10-13 11:53:19 +0900

    3..

---
+++ CH02_Variables/src/integer/App.java
@@ -0,0 +1,17 @@
+package integer;
+
+public class App {
+
+	public static void main(String[] args) {
+		// 변수 만들기 : 1. 변수 선언 (자료형) 2. 변수 초기화
+		int x; // 정수 자료형 선
+		x = 7; // 값 초기
+		int y = 7; // 정수형 10 초기화
+		int z = x + y;
+
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+
+}
