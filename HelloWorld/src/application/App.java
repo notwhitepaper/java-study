959d49773b17016ea31956473d85057567f0e5c0
SEUNG <app.lnob@gmail.com>
2021-10-13 11:16:30 +0900

    첫번째 commit

---
+++ HelloWorld/src/application/App.java
@@ -0,0 +1,10 @@
+package application; // 패키지 선언
+
+public class App {  // 클래스 선
+
+	public static void main(String[] args) {
+		// 메인 메소드에서 프로그램 시작  
+	System.out.println("헬로우 월드!"); // 아래 콘솔에 출력하는 메소
+	}
+
+}
---
+++ MainMethod/src/application/App.java
@@ -0,0 +1,12 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("안녕하세요 ");
+System.out.println("새 자바 프로그램을 ");
+System.out.println("실행 합니다. ");
+	}
+
+}
