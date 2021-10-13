87b26d51bddc7b9d5b18f1941e7423963730e877
SEUNG <app.lnob@gmail.com>
2021-10-13 12:29:53 +0900

    10/13 오전

---
+++ CH02_Variables/src/convertingTemp/App.java
@@ -0,0 +1,14 @@
+package convertingTemp;
+
+public class App {
+
+	public static void main(String[] args) {
+		// 섭씨 -> 화씨
+		double c = 25;
+		double f = (c * 9 / 5) + 32;
+		
+		System.out.println("섭씨 "+c+"는 화씨 "+f+"이다.");
+
+	}
+
+}
